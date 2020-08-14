2afb06fb9aa098fb2b22dbbcf40658efb054e6f7
foreknowledge <kyj_0129@naver.com>
2020-08-14 11:24:03 +0900

    feat: inital settings - 필요한 색상 추가 - NoStatusBar & NoActionBar인 가로모드 액티비티로 설정

--- app/src/main/java/com/ellie/billiardsgame/MainActivity.kt
+++ app/src/main/java/com/ellie/billiardsgame/MainActivity.kt
@@ -2,10 +2,18 @@ package com.ellie.billiardsgame
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setNoStatusBar()
         setContentView(R.layout.activity_main)
+    }
+
+    private fun setNoStatusBar() {
+        requestWindowFeature(Window.FEATURE_NO_TITLE)
+        this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
     }
 }
