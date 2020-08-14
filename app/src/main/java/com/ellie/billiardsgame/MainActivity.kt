8e7173b3c3bf8a40c7f6adcefd3789972504cb27
foreknowledge <kyj_0129@naver.com>
2020-08-14 16:22:09 +0900

    feat: 흰색 공 움직이는 기능 추가

--- app/src/main/java/com/ellie/billiardsgame/MainActivity.kt
+++ app/src/main/java/com/ellie/billiardsgame/MainActivity.kt
@@ -1,19 +1,47 @@
 package com.ellie.billiardsgame
 
-import androidx.appcompat.app.AppCompatActivity
+import android.annotation.SuppressLint
 import android.os.Bundle
+import android.view.GestureDetector
+import android.view.MotionEvent
 import android.view.Window
 import android.view.WindowManager
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.GestureDetectorCompat
+import kotlinx.android.synthetic.main.activity_main.*
 
+@SuppressLint("ClickableViewAccessibility")
 class MainActivity : AppCompatActivity() {
+    private val gestureDetector by lazy {
+        GestureDetectorCompat(this, object: GestureDetector.SimpleOnGestureListener() {
+            override fun onScroll(e1: MotionEvent?, e2: MotionEvent?, distanceX: Float, distanceY: Float): Boolean {
+                e2?.let { event ->
+                    whiteBall.x = event.rawX - whiteBall.width / 2
+                    whiteBall.y = event.rawY - whiteBall.width / 2
+                }
+
+                return true
+            }
+        })
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setNoStatusBar()
         setContentView(R.layout.activity_main)
+
+        whiteBall.setOnTouchListener { v, event ->
+           gestureDetector.onTouchEvent(event)
+            true
+        }
     }
 
     private fun setNoStatusBar() {
         requestWindowFeature(Window.FEATURE_NO_TITLE)
         this.window.setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN)
+    }
+
+    companion object {
+        private const val TAG = "MainActivity"
     }
 }
